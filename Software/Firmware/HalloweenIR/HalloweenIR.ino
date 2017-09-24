73f11f09e1e49153c380b288d9d225095821a94f
mike parks <mike@greenshoegarage.com>
2017-09-24 19:11:43 -0400

    v0.1 alpha code release
    all basic functionality completed

--- Software/Firmware/HalloweenIR/HalloweenIR.ino
+++ Software/Firmware/HalloweenIR/HalloweenIR.ino
@@ -1,50 +1,116 @@
-/*
-   IRremote: IRrecvDump - dump details of IR codes with IRrecv
-   An IR detector/demodulator must be connected to the input RECV_PIN.
-   Version 0.1 July, 2009
-   Copyright 2009 Ken Shirriff
-   http://arcfn.com
-   JVC and Panasonic protocol added by Kristian Lauszus (Thanks to zenwheel and other people at the original blog post)
-   RCMM protocol added by Matthias Neeracher
+/* Project: HalloweenIR
+ * Description: 
+ * 
+ * Attribution: IRremote libraries; Ken Shirriff, Kristian Lauszus, Matthias Neeracher
+ * License: GNU Lesser General Public License v2.1
 */
 
 #include <IRremote.h>
+#include <Servo.h>
 
+const uint16_t SERVO_PIN = 3;
+const uint16_t LED1_PIN = 5;
+const uint16_t LED2_PIN = 6;
+const uint16_t IRRECV_PIN = 8;
+const uint16_t MAGICWAND_SERIAL_NUM = 153312897; //153312897 is the decimal value of 0x9235E81
 
-int RECV_PIN = 11;
-
-IRrecv irrecv(RECV_PIN);
+IRrecv irrecv(IRRECV_PIN);
 decode_results results;
 
+Servo pumpkinServo;
+const uint16_t pumpkinOpenPosition = 0;
+const uint16_t pumpkinClosedPosition = 180;
+
+
+/*
+ * 
+ */
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("starting...");
+  Serial.print(F("Homebrewed IR Magic Wand System 1.0 initializing..."));
+
+  pinMode(LED1_PIN, OUTPUT);
+  pinMode(LED2_PIN, OUTPUT);
+  pinMode(SERVO_PIN, OUTPUT);
+  
   irrecv.enableIRIn(); // Start the receiver
+  
+  pumpkinServo.attach(SERVO_PIN);
+  pumpkinServo.write(pumpkinClosedPosition);
+  delay(15);
+  Serial.println(F("READY!"));
 }
 
 
-// Dumps out the decode_results structure.
-// Call this after IRrecv::decode()
-// void * to work around compiler issue
-//void dump(void *v) {
-//  decode_results *results = (decode_results *)v
-void dump(decode_results *results) {
+
+/*
+ * 
+ */
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    decodeIR(&results);
+    irrecv.resume(); // Receive the next value
+  }
+}
+
+
+
+
+/*
+ * 
+ */
+void decodeIR(decode_results *results) {
 
   int count = results->rawlen;
 
   if (results->decode_type == MAGIQUEST) {
-    Serial.println("  |-> MAGIQUEST");
-    if (results->value == 0x9235E81) {
-      Serial.println("     |-> MIKE WAND");
+    Serial.println(F("  |-> MAGIQUEST"));
+    if ((int)results->value == MAGICWAND_SERIAL_NUM) {
+      Serial.println(F("     |-> MIKE WAND"));
+      turnLEDEyesOn();
+      operatePumpkinServo(pumpkinOpenPosition);
+      delay(100);
+      turnLEDEyesOff();
+      delay(100);
+      operatePumpkinServo(pumpkinClosedPosition); 
+      delay(100);  
     }
   }
 }
 
-void loop() {
-  if (irrecv.decode(&results)) {
-    Serial.println(results.value, HEX);
-    dump(&results);
-    irrecv.resume(); // Receive the next value
-  }
+
+
+/*
+ * 
+ */
+void turnLEDEyesOn() {
+  Serial.println(F("Turning RED EYES ON!"));
+  digitalWrite(LED1_PIN, HIGH);
+  digitalWrite(LED2_PIN, HIGH);
 }
+
+
+
+/*
+ * 
+ */
+void turnLEDEyesOff() {
+  Serial.println(F("Turning RED EYES OFF!"));
+  digitalWrite(LED1_PIN, LOW);
+  digitalWrite(LED2_PIN, LOW);
+}
+
+
+
+/*
+ * 
+ */
+void operatePumpkinServo(const uint16_t desiredServoPosition) {
+  Serial.print(F("Moving pumpkin head to position: "));
+  Serial.println(desiredServoPosition);
+  pumpkinServo.write(desiredServoPosition);
+  delay(15);
+}
+
